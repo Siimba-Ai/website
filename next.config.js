f1db37dcf33599f417b7eecf0d0a8bfd215eec39
pranathi6678 <pranathi.kollolli@gmail.com>
2026-01-26 17:36:06 -0800

    Revert to standard Next.js config for Vercel

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  // For GitHub Pages with custom domain
-  basePath: '',
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
